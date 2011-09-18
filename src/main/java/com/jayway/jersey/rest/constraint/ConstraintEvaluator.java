0cb8cf95eb38008141d9b93d4da1d63addf434e1
Mads Enevoldsen <mads.enevoldsen@gmail.com>
2011-09-18 14:22:56 +0200

    removed old src

--- src/main/java/com/jayway/jersey/rest/constraint/ConstraintEvaluator.java
+++
@@ -1,8 +0,0 @@
-package com.jayway.jersey.rest.constraint;
-
-/**
- */
-public interface ConstraintEvaluator<Annotation, ContextMap> {
-
-    boolean isValid(Annotation annotation, ContextMap context);
-}
