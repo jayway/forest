f652118ed56d59602ac5e21725deb55acb4b01c4
Mads Enevoldsen <mads.enevoldsen@gmail.com>
2011-10-26 08:01:48 +0200

    added js library for history tracking

--- core/src/test/java/com/jayway/forest/core/CreatableTest.java
+++ core/src/test/java/com/jayway/forest/core/CreatableTest.java
@@ -3,9 +3,13 @@ package com.jayway.forest.core;
 import com.jayway.forest.service.AbstractRunner;
 import com.jayway.forest.service.StateHolder;
 import com.jayway.restassured.RestAssured;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static com.jayway.restassured.RestAssured.basePath;
+import static com.jayway.restassured.RestAssured.baseURI;
 import static com.jayway.restassured.RestAssured.given;
 
 /**
@@ -15,10 +19,14 @@ public class CreatableTest extends AbstractRunner {
     @Test
     public void testCreate() {
         String result = given().body("{\"string\":\"Hello\", \"integer\": 25}").
-        expect().statusCode( 201 ).header("Location", RestAssured.baseURI + RestAssured.basePath + "/other/Hello/" + 25 + "/")
+        expect().statusCode( 201 ).header("Location", baseURI + basePath + "/other/Hello/" + 25 + "/")
                 .when().post("/other/").andReturn().asString();
 
-        //Assert.assertEquals( "", result );
+        JSONObject o = (JSONObject) JSONValue.parse(result);
+        Assert.assertEquals( String.format("%s%s/other/Hello/25/", baseURI, basePath), o.get("href") );
+        Assert.assertEquals( "GET", o.get("method") );
+        Assert.assertEquals( "Hello", o.get("name") );
+        Assert.assertEquals( "appendabletest", o.get("rel") );
     }
 
     @Test
@@ -31,10 +39,10 @@ public class CreatableTest extends AbstractRunner {
     @Test
     public void testCreateHtml() {
         String result = given().spec( contentTypeFormUrlEncoded() ).expect().statusCode(201)
-                .header("Location", RestAssured.baseURI + RestAssured.basePath + "/other/hello/" + 25 + "/")
+                .header("Location", baseURI + basePath + "/other/hello/" + 25 + "/")
                 .post("/other/?argument1.StringAndIntegerDTO.string=hello&argument1.StringAndIntegerDTO.integer=25").andReturn().asString();
 
-        String expect = String.format("<code>Location:</code> <a href='%s/bank/other/hello/25/' rel='appendabletest'>hello</a>", RestAssured.baseURI);
+        String expect = String.format("<code>Location:</code> <a href='%s/bank/other/hello/25/' rel='appendabletest'>hello</a>", baseURI);
         Assert.assertEquals( expect, result );
     }
 }
--- samples/bank/bank-grove/src/main/webapp/js/lumberjack-0.3.js
+++ samples/bank/bank-grove/src/main/webapp/js/lumberjack-0.3.js
@@ -28,4 +28,48 @@ jQuery(document).ready(function()
 
     initializeStepCache( "/bank/" );
     $('#master').load('html/root.html' );
+
+
+
+    $(function(){
+        // Override the default behavior of all `a` elements so that, when
+        // clicked, their `href` value is pushed onto the history hash
+        // instead of being navigated to directly.
+        $("a").click(function(){
+            var href = $(this).attr( "href" );
+
+            // Push this URL "state" onto the history hash.
+            $.bbq.pushState({ url: href });
+
+            // Prevent the default click behavior.
+            return false;
+        });
+
+        // Bind a callback that executes when document.location.hash changes.
+        $(window).bind( "hashchange", function(e) {
+            // In jQuery 1.4, use e.getState( "url" );
+            var url = $.bbq.getState( "url" );
+
+            // In this example, whenever the event is triggered, iterate over
+            // all `a` elements, setting the class to "current" if the
+            // href matches (and removing it otherwise).
+            $("a").each(function(){
+            var href = $(this).attr( "href" );
+
+            if ( href === url ) {
+                $(this).addClass( "current" );
+            } else {
+                $(this).removeClass( "current" );
+            }
+        });
+
+    // You probably want to actually do something useful here..
+    });
+
+    // Since the event is only triggered when the hash changes, we need
+    // to trigger the event now, to handle the hash the page may have
+    // loaded with.
+    $(window).trigger( "hashchange" );
+    });
 })
+
