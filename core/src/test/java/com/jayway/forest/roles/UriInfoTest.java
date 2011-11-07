c175c771ba739b9d21a04e8cec393d650179a4ee
Ulrik Sandberg <ulrik.sandberg@jayway.com>
2011-11-07 22:47:12 +0100

    Fixed problem with UriInfo when path is empty.

--- core/src/test/java/com/jayway/forest/roles/UriInfoTest.java
+++ core/src/test/java/com/jayway/forest/roles/UriInfoTest.java
@@ -9,10 +9,11 @@ import static org.junit.Assert.assertEquals;
 public class UriInfoTest {
 
     private UriInfo tested;
+    private MockHttpServletRequest request;
 
     @Before
     public void setUp() {
-        MockHttpServletRequest request = new MockHttpServletRequest(null, "/mywebapp/servlet/MyServlet/a/b;c=123");
+        request = new MockHttpServletRequest(null, "/mywebapp/servlet/MyServlet/a/b;c=123");
         request.setServletPath("/servlet/MyServlet");
         request.setPathInfo("/a/b;c=123");
 
@@ -39,5 +40,14 @@ public class UriInfoTest {
         tested.addPath("apa");
         assertEquals("apa/", tested.getRelativeUrl());
         assertEquals("http://localhost:80/mywebapp/servlet/MyServlet/apa/", tested.getSelf());
+    }
+
+    @Test
+    public void emptyPathInfoShouldNotCauseError() throws Exception {
+        request = new MockHttpServletRequest(null, "/mywebapp/servlet/MyServlet/");
+        request.setServletPath("/servlet/MyServlet");
+        request.setPathInfo(null);
+        tested = new UriInfo(request);
+        assertEquals("http://localhost:80/mywebapp/servlet/MyServlet/", tested.getBaseUrl());
     }
 }
