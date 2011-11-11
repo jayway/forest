c1bc17c06b7397339a6ff23b0dd931d1c3e6c818
Mads Enevoldsen <mads.enevoldsen@gmail.com>
2011-11-11 07:01:47 +0100

    small changes

--- samples/bank/bank-grove/src/main/webapp/js/lumberjack-0.3.js
+++ samples/bank/bank-grove/src/main/webapp/js/lumberjack-0.3.js
@@ -30,6 +30,42 @@ jQuery(document).ready(function()
     $('#master').load('html/root.html' );
 
 
+    function executeScenario( steps ) {
+        // commands executes syncronously
+        // queries async
+
+        // Issues: user input
+        // -> must implement a dialog handler
+        // for the steps that needs input
+
+        // Issues: method argument from other queries
+        // -> the input handler can be coded to
+        // accept output from named queries
+
+        // When are we done?
+        // unknown. Maybe configure a global
+        // timeout
+
+        // When does the state machine crash?
+        // -> when a step cannot be found and
+        // we are done executing all previous
+        // steps
+
+
+    }
+
+    function evaluatePath( path ) {
+        // break down in segments and evaluate through
+        // the view path
+
+        // each segment represents a view
+        // == { construct, view, }
+
+
+    }
+
+
+
 
     $(function(){
         // Override the default behavior of all `a` elements so that, when
@@ -65,6 +101,38 @@ jQuery(document).ready(function()
 
     // You probably want to actually do something useful here..
     });
+
+    var viewTree = [ rootView,
+        { "idView" : accountView,
+          "subViews" : {
+
+          }
+        }
+    ];
+
+    var rootView = {
+        "view"  : "root",
+        "construct" : ["AccountsResourceDiscover"],
+        "subViews" :
+    };
+
+    var accountView = {
+        "view" : "accountView",
+        "steps": [ "AccountsResourceId" ],
+        "construct" : [ "AccountResourceTransactions" ]
+    };
+
+    var transactionView = {
+        "view" : "transactionView",
+        "steps" : [ "AccountResourceId" ]
+    }
+
+    // views are bookmarkable - e.g. show account
+    // interactions are not - e.g. transfer money
+    var transferMoneyInteraction = {
+        "view" : "TransferMoneyInteraction",
+        "steps" : [ "AccountsResourceId", "AccountResourceTransfer", "" ]
+    }
 
     // Since the event is only triggered when the hash changes, we need
     // to trigger the event now, to handle the hash the page may have
