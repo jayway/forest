05c9c353c3ad3ae95cffb22f707a5f2333279077
Ulrik Sandberg <ulrik.sandberg@jayway.com>
2011-11-29 12:15:45 +0100

    Fixed problem in UriInfo when non-ascii path segments had been url-encoded.

--- core/src/test/java/com/jayway/forest/roles/UriInfoTest.java
+++ core/src/test/java/com/jayway/forest/roles/UriInfoTest.java
@@ -4,6 +4,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import java.net.URLEncoder;
+
 import static org.junit.Assert.assertEquals;
 
 public class UriInfoTest {
@@ -13,9 +15,11 @@ public class UriInfoTest {
 
     @Before
     public void setUp() {
-        request = new MockHttpServletRequest(null, "/mywebapp/servlet/MyServlet/a/b;c=123");
-        request.setServletPath("/servlet/MyServlet");
-        request.setPathInfo("/a/b;c=123");
+        final String servletPath = "/servlet/MyServlet";
+        final String pathInfo = "/a/b;c=123";
+        request = new MockHttpServletRequest(null, "/mywebapp" + servletPath + pathInfo);
+        request.setServletPath(servletPath);
+        request.setPathInfo(pathInfo);
 
         tested = new UriInfo(request);
     }
@@ -47,6 +51,17 @@ public class UriInfoTest {
         request = new MockHttpServletRequest(null, "/mywebapp/servlet/MyServlet/");
         request.setServletPath("/servlet/MyServlet");
         request.setPathInfo(null);
+        tested = new UriInfo(request);
+        assertEquals("http://localhost:80/mywebapp/servlet/MyServlet/", tested.getBaseUrl());
+    }
+
+    @Test
+    public void urlEncodedPathShouldNotCauseError() throws Exception {
+        String encodedPathInfo = "/administration/campaigns/" + URLEncoder.encode("åäö", "UTF-8");
+        String servletPath = "/servlet/MyServlet";
+        request = new MockHttpServletRequest(null, "/mywebapp" + servletPath + encodedPathInfo);
+        request.setServletPath(servletPath);
+        request.setPathInfo("/administration/campaigns/åäö");
         tested = new UriInfo(request);
         assertEquals("http://localhost:80/mywebapp/servlet/MyServlet/", tested.getBaseUrl());
     }
