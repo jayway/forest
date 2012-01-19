74f8e2777152d7fa529430b04c2d29a7eef93c4d
Jan Kronquist <jan.kronquist@gmail.com>
2012-01-19 13:14:15 +0100

    Removed unused sample project

--- samples/bank/bank-plain-jersey/src/main/java/com/jayway/forest/core/Setup.java
+++
@@ -1,9 +0,0 @@
-package com.jayway.forest.core;
-
-import com.jayway.forest.di.grove.GroveDependencyInjectionImpl;
-
-public class Setup {
-	public static void setup() {
-		RoleManager.spi = new GroveDependencyInjectionImpl();
-	}
-}
